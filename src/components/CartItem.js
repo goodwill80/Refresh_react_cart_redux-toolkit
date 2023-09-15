229c4e09a1d8398a5fc9ae593d3b06561d7a0638
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-09-16 00:08:55 +0800

    setup components

---
+++ src/components/CartItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CartItem() {
+  return <div>Cart Item</div>;
+}
+
+export default CartItem;
--- src/icons.js
+++ src/icons.js
@@ -1,17 +1,19 @@
+// Source icons - https://heroicons.com/
+
 export const CartIcon = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6 '
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6 "
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
       <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        d='M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z'
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
       />
     </svg>
   );
@@ -20,14 +22,14 @@ export const CartIcon = () => {
 export const ChevronDown = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
-      <path strokeLinecap='round' strokeLinejoin='round' d='M19 9l-7 7-7-7' />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
     </svg>
   );
 };
@@ -35,14 +37,33 @@ export const ChevronDown = () => {
 export const ChevronUp = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
-      <path strokeLinecap='round' strokeLinejoin='round' d='M5 15l7-7 7 7' />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M5 15l7-7 7 7" />
     </svg>
   );
 };
+
+// export const Testing = () => {
+//   return (
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       fill="none"
+//       viewBox="0 0 24 24"
+//       strokeWidth={1.5}
+//       stroke="currentColor"
+//       className="w-6 h-6"
+//     >
+//       <path
+//         strokeLinecap="round"
+//         strokeLinejoin="round"
+//         d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+//       />
+//     </svg>
+//   );
+// };
