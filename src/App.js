86d1ba494d77abf379b2dfc17e25916bb0de6182
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-09-16 13:49:46 +0800

    implemented axios and added notes on use of thunkAPI

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import Navbar from './components/navbar';
 import CartContainer from './components/CartContainer';
 import Modal from './components/Modal';
 import { useDispatch, useSelector } from 'react-redux';
-import { calculateTotals } from './features/cart/cartSlice';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 
 function App() {
   const dispatch = useDispatch();
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems('can_be_accessed'));
+  }, []);
+
+  if (isLoading) {
+    return <div className="loading">Loading...</div>;
+  }
 
   return (
     <main>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,17 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
-import cartItems from '../../cartItems';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import cartItems from '../../cartItems';
+import axios from 'axios';
+import { openModal } from '../modal/modalSlice';
 
+// URL
+const url = 'https://course-api.com/react-useReducer-cart-project';
+
+// Initial State
 const initialState = {
-  cartItems: [...cartItems],
+  cartItems: [],
   amount: 5,
   total: 0,
   isLoading: true,
 };
 
+// API fetch using asyncThunk
+export const getCartItems = createAsyncThunk(
+  'cart/getCartItems',
+  // Can have 2 parameters
+  async (testing, thunkAPI) => {
+    try {
+      // You can pass in args
+      console.log(testing);
+      // You can retrieve the thunk object which has lots of valuable roperties
+      console.log(thunkAPI);
+      // You can access all the states in the entire redux store
+      console.log(thunkAPI.getState());
+      // You can also perform dispatch here
+      //  thunkAPI.dispatch(openModal());
+      // Call API to return the data
+      const resp = await axios(url);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('something went wrong!');
+    }
+  }
+);
+
 // Reducer actions - "No more reference error" we no longer need to return a new state due to immer library which do all the heavy lifting
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
+
+  // Local State Management
   reducers: {
     clearCart: (state) => {
       state.cartItems = [];
@@ -38,6 +69,22 @@ const cartSlice = createSlice({
       });
       state.amount = amount;
       state.total = total;
+    },
+  },
+
+  // State Management via API AsyncThunk
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      console.log(action);
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state, action) => {
+      console.log(action.payload);
+      state.isLoading = false;
     },
   },
 });
